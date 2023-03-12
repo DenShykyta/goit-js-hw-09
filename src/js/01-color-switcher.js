3b0abf9209fbcce805b89e0168e963279e049ee0
DenShykyta <wrkacc4@gmail.com>
2023-03-12 21:32:34 +0200

    number 1 (done)

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+    start: document.querySelector('button[data-start]'),
+    stop: document.querySelector('button[data-stop]'),
+}
 
+
+refs.start.addEventListener('click', onSwitcherStart);
+refs.stop.addEventListener('click', onSwitcherStop);
+
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onSwitcherStart() {
+    intervalId = setInterval(() => {
+    window.document.body.style.background = getRandomHexColor();
+    }, 1000);
+    refs.start.disabled = true;
+    refs.stop.disabled = false;
+}
+
+function onSwitcherStop() {
+    clearInterval(intervalId);
+    refs.stop.disabled = true;
+    refs.start.disabled = false;
+ }
